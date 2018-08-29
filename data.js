ffcd566277dc3cd08bc86edeb8dbf4e5c78b0ca0
timegold-websrc <39736627+timegold-websrc@users.noreply.github.com>
2018-08-29 22:03:04 +0800

    Angular-Stacked-BarChart

---
+++ data.js
@@ -0,0 +1,67 @@
+
+
+var cities = ["Amsterdam", "Rotterdm"]
+
+var data = [
+    {
+        "year":"2015",
+        "population":{"Amsterdam":"20", "Rotterdm":"60"}
+    },
+    {
+        "year":"2016",
+        "population":{"Amsterdam":"50", "Rotterdm":"50"}
+    },
+    {
+        "year":"2012",
+        "population":{"Amsterdam":"60", "Rotterdm":"20"}
+    },
+    {
+        "year":"2017",
+        "population":{"Amsterdam":"10", "Rotterdm":"20"}
+    },
+    {
+        "year":"2018",
+        "population":{"Amsterdam":"25", "Rotterdm":"40"}
+    },
+    {
+        "year":"2013",
+        "population":{"Amsterdam":"80", "Rotterdm":"70"}
+    },
+    {
+        "year":"2011",
+        "population":{"Amsterdam":"30", "Rotterdm":"90"}
+    },
+    {
+        "year":"2014",
+        "population":{"Amsterdam":"30", "Rotterdm":"20"}
+    },
+    {
+        "year":"2010",
+        "population":{"Amsterdam":"65", "Rotterdm":"30"}
+    },
+    {
+        "year":"2009",
+        "population":{"Amsterdam":"30", "Rotterdm":"90"}
+    },
+    {
+        "year":"2008",
+        "population":{"Amsterdam":"30", "Rotterdm":"20"}
+    },
+    {
+        "year":"2007",
+        "population":{"Amsterdam":"65", "Rotterdm":"30"}
+    }
+];
+
+// function getJsonData(){
+//     var from = "2011";
+//     var to = "2012";
+//     var filter_data=[];
+//     for(var index in newdata){
+//         var data = newdata[index];
+//         if(data.year >= from && data.year <= to){
+//             filter_data.push(data);
+//         }
+//     }
+//     console.log(filter_data);
+// }
